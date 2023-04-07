b2a9dbe30b42dc4fdda6d1e3a418e08fba95fa5b
Hítalo Rodrigues <hitalo005@gmail.com>
2023-04-07 18:54:41 -0300

    Aulas do projeto finalizadas

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import { HomeContainer, Product } from "../styles/pages/home"
 import Image from "next/image"
 import Link from "next/link"
+import Head from "next/head";
 
 import { useKeenSlider } from 'keen-slider/react';
 
@@ -27,22 +28,28 @@ export default function Home({ products }: HomeProps) {
   })
 
   return (
-    <HomeContainer ref={sliderRef} className="keen-slider">
-      {products.map(product => {
-        return (
-          <Link key={product.id} href={`/product/${product.id}`} >
-            <Product className="keen-slider__slide">
-              <Image src={product.imageUrl} width={520} height={480} alt="camiseta1" />
-              <footer>
-                <strong>{product.name}</strong>
-                <span>{product.price}</span>
-              </footer>
-            </Product>
-          </Link>
-        )
-      })}
+    <>
+      <Head>
+        <title>Home | Ignite Shop</title>
+      </Head>
 
-    </HomeContainer>
+      <HomeContainer ref={sliderRef} className="keen-slider">
+        {products.map(product => {
+          return (
+            <Link key={product.id} href={`/product/${product.id}`} prefetch={false}>
+              <Product className="keen-slider__slide">
+                <Image src={product.imageUrl} width={520} height={480} alt="camiseta1" />
+                <footer>
+                  <strong>{product.name}</strong>
+                  <span>{product.price}</span>
+                </footer>
+              </Product>
+            </Link>
+          )
+        })}
+
+      </HomeContainer>
+    </>
   )
 }
 
--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,7 +1,11 @@
 import { stripe } from '@/lib/stripe';
 import { ImageContainer, ProductContainer, ProductDetails } from '@/styles/pages/product';
+import axios from 'axios';
 import { GetStaticPaths, GetStaticProps } from 'next';
+import Head from 'next/head';
 import Image from 'next/image';
+import { useState } from 'react';
+//import { useRouter } from 'next/router';
 import Stripe from 'stripe';
 
 interface ProductProps {
@@ -10,13 +14,47 @@ interface ProductProps {
         name: string;
         imageUrl: string;
         price: string;
-        description: number;    
+        description: number;
+        defaultPriceId: string;    
     }
 }
 
 export default function Product({ product }: ProductProps) {
+    const [isCreatingCheckoutSession, setIsCreatingCheckoutSession] = useState(false)
+    //const router = useRouter()   
+    async function handleBuyProduct() {
+        try {
+            setIsCreatingCheckoutSession(true);
+
+            const response = await axios.post('/api/checkout', {
+                priceId: product.defaultPriceId,
+            })
+
+            const { checkoutUrl } = response.data;
+
+            //router.push('/checkout') //página interna
+
+            window.location.href = checkoutUrl //página externa
+        } catch (err) {
+            // Conectar com uma ferramenta de observabilidade (Datadog / Sentry)
+            setIsCreatingCheckoutSession(false);
+
+            alert('Falha ao redirecionar ao checkout!')
+        }
+    }
+    // const { isFallback } = useRouter()
+
+    // if (isFallback) {
+    //     return <p>Loading...</p>
+    // }
 
     return (
+    <>
+        <Head>
+            <title>{product.name} | Ignite Shop</title>
+        </Head>
+
+    
         <ProductContainer>
             <ImageContainer>
                 <Image src={product.imageUrl} width={520} height={520} alt="imagem do produto"/>
@@ -25,18 +63,22 @@ export default function Product({ product }: ProductProps) {
                 <h1>{product.name}</h1>
                 <span>{product.price}</span>
                 <p>{product.description}</p>
-                <button>Comprar agora</button>
+                <button disabled={isCreatingCheckoutSession} onClick={handleBuyProduct}>
+                    Comprar agora
+                </button>
             </ProductDetails>
         </ProductContainer>
+    </>
     )
 }
 
 export const getStaticPaths: GetStaticPaths = async () => {
+    // Optar buscar os produtos mais vendidos / mais acessados
     return {
         paths: [
-            { params: { id: 'prod_Nd3HicuiPzjz5P' } }
+            { params: { id: 'prod_Nd3HicuiPzjz5P' } } //cuidado para manter o build leve
         ],
-        fallback: false,
+        fallback: true,
     }
 }
 
@@ -60,6 +102,7 @@ export const getStaticProps: GetStaticProps<any, { id: string }> = async ({ para
                     currency: 'BRL',
                 }).format(price.unit_amount ? price.unit_amount / 100 : 0),
                 description: product.description,
+                defaultPriceId: price.id,
             }
         },
         revalidate: 60 * 60 * 1, // 1 hour
